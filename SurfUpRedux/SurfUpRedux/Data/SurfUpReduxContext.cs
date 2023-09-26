285b27572daf725da21f0d2d7458e914cad523ce
Andreas Pedersen <andreaspede@protonmail.com>
2023-09-26 19:20:08 +0200

    SurfUpRedux

---
+++ SurfUpRedux/SurfUpRedux/Data/SurfUpReduxContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SurfUpRedux.Models;
+
+namespace SurfUpRedux.Data
+{
+    public class SurfUpReduxContext : DbContext
+    {
+        public SurfUpReduxContext (DbContextOptions<SurfUpReduxContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Board> Board { get; set; } = default!;
+    }
+}
---
+++ SurfUpRedux/SurfUpRedux/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SurfUpRedux.Data;
+using MvcMovie.Models;
+
+namespace SurfUpRedux
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<SurfUpReduxContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SurfUpReduxContext") ?? throw new InvalidOperationException("Connection string 'SurfUpReduxContext' not found.")));
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                SeedData.Initialize(services);
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.UseRequestLocalization("da-DK");
+
+            app.Run();
+        }
+    }
+}
