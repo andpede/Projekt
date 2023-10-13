185a1388a9445eb7fcd91d94a3291c1047982bb8
Johnny Hoang Dinh Huy Nguyen <jhdn48823@edu.ucl.dk>
2023-10-13 18:14:47 +0200

    Small changes

--- SurfUpRedux/SurfUpRedux/Controllers/BookingsController.cs
+++ SurfUpRedux/SurfUpRedux/Controllers/BookingsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SurfUpRedux.Data;
 using SurfUpRedux.Models;
@@ -66,97 +67,6 @@ namespace SurfUpRedux.Controllers
         }
 
         // GET: Bookings/Create
-        //public async Task<IActionResult> Create(int boardId)
-        //{
-
-        //    //var userId = _userManager.GetUserId(User);
-
-        //    //{
-        //    //    booking.UserId = userId;
-        //    //    booking.BoardId = boardId;
-        //    //    booking.BookingS = DateTime.Now;
-        //    //    booking.BookingEnd = DateTime.Now;
-        //    //};
-        //    //return View(booking);
-
-        //    if (User.IsInRole("Admin") || User.IsInRole("Manager"))
-        //    {
-        //        ViewData["BoardId"] = new SelectList(_context.Board, "Id", "Name");
-        //        ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email");
-
-        //    }
-        //    else
-        //    {
-
-        //        var user = await _userManager.GetUserAsync(User);
-        //        ViewData["UserId"] = new SelectList(new List<SurfUpUser> { user }, "Id", "Email");
-
-
-        //        var board = await _context.Board.FindAsync(boardId);
-        //        if (board != null)
-        //        {
-        //            ViewData["BoardId"] = new SelectList(new List<Board> { board }, "Id", "Name");
-        //        }
-        //        else
-        //        {
-
-        //            return NotFound(); 
-        //        }   
-
-        //    }
-
-        //    return View();
-        //}
-
-        //// POST: Bookings/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("StartDate,EndDate,BoardId,UserId")] Booking booking)
-        //{
-        //    ModelState.Remove("Board");
-        //    ModelState.Remove("User");
-
-        //    foreach (var modelState in ModelState.Values)
-        //    {
-        //        foreach (var error in modelState.Errors)
-        //        {
-        //            Console.WriteLine(error.ErrorMessage);
-        //        }
-        //    }
-
-        //    if (booking.StartDate > booking.EndDate)
-        //    {
-        //        ModelState.AddModelError("StartDate", "Startdate has to be before enddate");
-        //    }
-        //    if (booking.EndDate >= booking.StartDate.AddDays(3))
-        //    {
-        //        ModelState.AddModelError("Enddate", "You can only book this board for 3 days");
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(booking);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["CurrentRowVersion"] = booking.RowVersion;
-
-        //    ViewData["BoardId"] = new SelectList(_context.Board, "Id", "Name", booking.BoardId);
-
-        //    if (User.IsInRole("Admin") || User.IsInRole("Manager")) 
-        //    {
-        //        ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", booking.UserId);
-        //    }
-        //    else if (User.IsInRole("User"))
-        //    {
-        //        var user = await _userManager.GetUserAsync(User);
-        //        ViewData["UserId"] = new SelectList(new List<SurfUpUser> { user }, "Id", "Email", booking.UserId);
-        //    }
-        //    return View(booking);
-        //}
-        // GET: Bookings/Create
         public async Task<IActionResult> Create(int boardId)
         {
             if (User.IsInRole("Admin") || User.IsInRole("Manager"))
@@ -179,7 +89,6 @@ namespace SurfUpRedux.Controllers
                     return NotFound();
                 }
             }
-
             // Create a new Booking with a new RowVersion
             var booking = new Booking
             {
@@ -187,7 +96,6 @@ namespace SurfUpRedux.Controllers
                 StartDate = DateTime.Now, // Set StartDate to the current date
                 EndDate = DateTime.Now
             };
-
             return View(booking);
         }
 
@@ -206,16 +114,30 @@ namespace SurfUpRedux.Controllers
             if (booking.EndDate >= booking.StartDate.AddDays(3))
             {
                 ModelState.AddModelError("EndDate", "You can only book this board for 3 days");
-            }
-
+            }        
             if (ModelState.IsValid)
             {
-                // Generate a new RowVersion for the new booking
-                booking.RowVersion = new byte[8];
-
-                _context.Add(booking);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    // Generate a new RowVersion for the new booking
+                    booking.RowVersion = new byte[8];
+                    _context.Add(booking);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException ex)
+                {
+                    if (ex.InnerException is SqlException sqlException && sqlException.Number == 2601)
+                    {
+                        // Handle the unique constraint violation, e.g., display an error message
+                        ModelState.AddModelError("BoardId", "A booking with this board already exists.");
+                    }
+                    else
+                    {
+                        // Handle other types of DbUpdateException
+                        throw;
+                    }
+                }
             }
 
             ViewData["BoardId"] = new SelectList(_context.Board, "Id", "Name", booking.BoardId);
@@ -232,64 +154,7 @@ namespace SurfUpRedux.Controllers
             return View(booking);
         }
 
-
-        // GET: Bookings/Edit/5
-        //[Authorize(Roles = "Admin,Manager")]
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null || _context.Booking == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var booking = await _context.Booking.FindAsync(id);
-        //    if (booking == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    ViewData["BoardId"] = new SelectList(_context.Board, "Id", "Name", booking.BoardId);
-        //    ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", booking.UserId);
-        //    return View(booking);
-        //}
-
-        //// POST: Bookings/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[Authorize(Roles = "Admin,Manager")]
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,StartDate,EndDate,BoardId,UserId")] Booking booking)
-        //{
-        //    if (id != booking.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(booking);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!BookingExists(booking.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["BoardId"] = new SelectList(_context.Board, "Id", "Name", booking.BoardId);
-        //    ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", booking.UserId);
-        //    return View(booking);
-        //}
-        // GET: Bookings/Edit/5
+        //GET: Bookings/Edit/5
         [Authorize(Roles = "Admin,Manager")]
         public async Task<IActionResult> Edit(int? id)
         {
@@ -305,19 +170,19 @@ namespace SurfUpRedux.Controllers
             }
 
             // Pass the current row version to the view
-            ViewData["CurrentRowVersion"] = booking.RowVersion;
+            //ViewData["CurrentRowVersion"] = booking.RowVersion;
 
             ViewData["BoardId"] = new SelectList(_context.Board, "Id", "Name", booking.BoardId);
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", booking.UserId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", booking.UserId);
             return View(booking);
         }
 
+
         // POST: Bookings/Edit/5
-      
         [Authorize(Roles = "Admin,Manager")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,StartDate,EndDate,BoardId,UserId,RowVersion")] Booking booking, byte[] currentRowVersion)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,StartDate,EndDate,BoardId,UserId")] Booking booking)
         {
             if (id != booking.Id)
             {
@@ -326,29 +191,28 @@ namespace SurfUpRedux.Controllers
 
             if (ModelState.IsValid)
             {
-                // Check for concurrency conflict
-                if (!currentRowVersion.SequenceEqual(booking.RowVersion))
+                try
                 {
-                    // Handle the concurrency conflict here, e.g., by displaying an error message
-                    ModelState.AddModelError("Concurrency", "Concurrency conflict detected. Another user has updated this booking.");
-                    ViewData["BoardId"] = new SelectList(_context.Board, "Id", "Name", booking.BoardId);
-                    ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", booking.UserId);
-                    return View(booking);
+                    _context.Update(booking);
+                    await _context.SaveChangesAsync();
                 }
-
-                // If there's no concurrency conflict, proceed with the update
-                _context.Update(booking);
-                await _context.SaveChangesAsync();
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookingExists(booking.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-
             ViewData["BoardId"] = new SelectList(_context.Board, "Id", "Name", booking.BoardId);
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", booking.UserId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", booking.UserId);
             return View(booking);
         }
-
-
-
 
         // GET: Bookings/Delete/5
         [Authorize(Roles = "Admin,Manager,User")]
